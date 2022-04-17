87250074973d12a9fa5d723370eaf6023a449a45
Reed James <reedtjames1234@gmail.com>
2022-04-17 14:57:35 -0500

    never pushed up original main

---
+++ BlockChain_Demo/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChain_Demo
+{
+    public class Program
+    {
+        // Source https://dev.to/amir_ashy/simplest-blockchain-in-c-1f70
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
